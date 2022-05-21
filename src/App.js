1d697c85f2029a22d11a2f4e71b59f03abef5ca1
V.Vikash <61134025+VVikash28@users.noreply.github.com>
2022-05-21 20:26:04 +0530

    Add files via upload
    Ui changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProductCatalog from "./component";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <ProductCatalog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component.js
@@ -0,0 +1,43 @@
+import { React } from "react";
+
+export default class ProductCatalog extends React.Component {
+  // Creates a ProductTile object to be rendered
+  renderTile = (current_item) => {
+    return <ProductTile product={current_item}></ProductTile>;
+  };
+
+  // Iterates through the product list and calls the renderTile
+  // method to create the ProductTile object
+  render() {
+    let tiles = [];
+    for (let i = 0; i < this.props.products.length; i++) {
+      const current_item = this.props.products[i];
+      tiles.push(this.renderTile(current_item));
+    }
+    return tiles;
+  }
+}
+
+// ProductTile: A Bulma box containing information about a specific product
+class ProductTile extends React.Component {
+  // Renders the ProductTile with Product Information
+  render() {
+    return (
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src={this.props.product.productUrl} alt="image"></img>
+          </figure>
+        </div>
+        <div class="card-content">
+          <p class="title product-title">{this.props.product.productName}</p>
+          <a
+            class="button is-primary"
+            href={"product.html?id=" + this.props.product.productId.toString()}
+            target="_blank"
+          ></a>
+        </div>
+      </div>
+    );
+  }
+}
